4628e221416d9da10375a2b0a860276245f9033b
HIH-0906 <homuihomu0906@gmail.com>
2021-04-14 11:15:24 +0900

    strをどう解析しようかな

--- 05_1916035/main.cpp
+++ 05_1916035/main.cpp
@@ -1,10 +1,33 @@
 #include <DxLib.h>
+#include <string>
+#include <fstream>
 
 #define SCREEN_SIZE_X 400
 #define SCREEN_SIZE_Y 400
+#define DIVISION_NUM 8
+#define DIVISION_X (SCREEN_SIZE_X / DIVISION_NUM)
+#define DIVISION_Y (SCREEN_SIZE_Y / DIVISION_NUM)
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+
+	auto ReedFile = [](const std::string filename,std::string& str) {
+		std::fstream file(filename);
+		if (!file.is_open())
+		{
+			return false;
+		}
+		std::getline(file, str);
+		return true;
+	};
+
+	auto CheckStr = [](const std::string str)
+	{
+		return str.find_first_not_of("0123456789yx+-*/=") == std::string::npos;
+	};
+
+
+
 	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 16);
 	ChangeWindowMode(true);
 	SetWindowText("–â5_1916035_‹´–{‘å‹P");
@@ -13,11 +36,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		return 0;
 	}
 
+	int a = 1;
+	int b = 0;
+
 	while (!ProcessMessage() && !CheckHitKey(KEY_INPUT_ESCAPE))
 	{
 		ClsDrawScreen();
+		std::string filename = "number5.txt";
+		std::string str = "";
+		if (!ReedFile(filename, str) || !CheckStr(str))
+		{
+			break;
+		}
+
+		for (int tmp = 0; tmp < 8; tmp++)
+		{
+			DrawLine(DIVISION_X * tmp, 0, DIVISION_X * tmp, SCREEN_SIZE_Y, 0x888888);
+			DrawLine(0, DIVISION_Y * tmp, SCREEN_SIZE_X, DIVISION_Y * tmp, 0x888888);
+		}
 		DrawLine(SCREEN_SIZE_X / 2, 0, SCREEN_SIZE_X / 2, SCREEN_SIZE_Y, 0xffff);
 		DrawLine(0, SCREEN_SIZE_Y / 2, SCREEN_SIZE_X, SCREEN_SIZE_Y / 2, 0xffff);
+
+		for (int x = 0; x < SCREEN_SIZE_X; x++)
+		{
+			DrawPixel(SCREEN_SIZE_X - x, a * (x - SCREEN_SIZE_X / 2) + (b * -DIVISION_X) + SCREEN_SIZE_X / 2, 0xffffff);
+		}
+
 		ScreenFlip();
 	}
 	DxLib_End();
