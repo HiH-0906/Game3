7b8b3d6bb202a5ef8da1f6d34b3e350a31aa6f1b
HIH-0906 <homuihomu0906@gmail.com>
2021-08-03 11:15:42 +0900

    ちょっとだけ

--- shootemup/shootemup/Trail.cpp
+++ shootemup/shootemup/Trail.cpp
@@ -10,6 +10,7 @@ namespace
 Trail::Trail(HomingShot& owner):
 	owner_(owner)
 {
+	handle_ = 0;
 }
 
 void Trail::Update()
@@ -26,12 +27,21 @@ void Trail::Draw()
 	auto oldPos = owner_.pos;
 	auto tickness = 10.0f;
 	float div = 1.0f / static_cast<float>(history_.size());
+	float u = 0.0f;
 
-	for (const auto& trail : history_)
+	for (const auto& pos : history_)
 	{
-		DrawLineAA(oldPos.x, oldPos.y, trail.x, trail.y, 0xffffff, tickness);
+		auto v = pos - oldPos;
+		v.Normalize();
+		v = Vector2f(-v.y, v.x);
+		auto p1 = oldPos + v * 16.0f;
+		auto p2 = pos + v * 16.0f;
+		auto p3 = pos - v * 16.0f;
+		auto p4 = oldPos - v * 16.0f;;
+		DrawRectModiGraph(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y, u * 256, 0, div * 256, 64, handle_, true);
+		u += div;
 		tickness *= 0.8f;
-		oldPos = trail;
+		oldPos = pos;
 	}
 }
 
